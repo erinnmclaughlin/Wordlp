e12819f268a9c9c3e79a904e52534801644c074c
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-08-05 11:20:57 -0400

    get random word

--- Wordlp/Models/WordCollection.cs
+++ Wordlp/Models/WordCollection.cs
@@ -3,4 +3,10 @@
 public class WordCollection
 {
     public List<Word> Words { get; set; } = new();
+
+    public Word GetRandomWord()
+    {
+        var randomIndex = new Random().Next(0, Words.Count - 1);
+        return Words.ElementAt(randomIndex);
+    }
 }
