db8a1470637622c159df5ada472c4361b25aaf03
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-08-08 13:17:00 -0400

    show word number in history card

--- Wordlp/Models/WordCollection.cs
+++ Wordlp/Models/WordCollection.cs
@@ -3,4 +3,10 @@
 public class WordCollection
 {
     public List<Word> Words { get; set; } = new();
+
+    public int GetNumber(string value)
+    {
+        var word = Words.First(w => w.Value == value);
+        return Words.IndexOf(word) + 1;
+    }
 }
