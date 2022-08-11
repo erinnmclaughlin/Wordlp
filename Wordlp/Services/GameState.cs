7a8549de4ab18df5edb610c3a481d6211e55c4c0
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-08-10 23:58:24 -0400

    game over modal opens itself

--- Wordlp/Services/GameState.cs
+++ Wordlp/Services/GameState.cs
@@ -2,7 +2,19 @@
 
 public class GameState
 {
+    public event EventHandler? OnGameOver;
+    public event EventHandler? OnGameStart;
     public event EventHandler? OnInvalidSubmit;
+
+    public void NotifyGameOver()
+    {
+        OnGameOver?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void NotifyGameStart()
+    {
+        OnGameStart?.Invoke(this, EventArgs.Empty);
+    }
 
     public void NotifyInvalidSubmit()
     {
