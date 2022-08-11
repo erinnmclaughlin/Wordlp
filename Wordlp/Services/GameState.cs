25dce57c32b374bfdf0ba463c799a61ef2aad949
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-08-10 23:11:51 -0400

    who needs game state anyway

--- Wordlp/Services/GameState.cs
+++ Wordlp/Services/GameState.cs
@@ -2,28 +2,10 @@
 
 public class GameState
 {
-    public event EventHandler? OnGameStart;
-    public event EventHandler? OnGameOver;
     public event EventHandler? OnInvalidSubmit;
-    public event EventHandler? OnValidSubmit;
-
-    public void NotifyGameStarted()
-    {
-        OnGameStart?.Invoke(this, EventArgs.Empty);
-    }
-
-    public void NotifyGameOver()
-    {
-        OnGameOver?.Invoke(this, EventArgs.Empty);
-    }
 
     public void NotifyInvalidSubmit()
     {
         OnInvalidSubmit?.Invoke(this, EventArgs.Empty);
-    }
-
-    public void NotifyValidSubmit()
-    {
-        OnValidSubmit?.Invoke(this, EventArgs.Empty);
     }
 }
