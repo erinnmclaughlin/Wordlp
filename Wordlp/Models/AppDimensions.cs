acd0c55a005af2564edd5668ef9c87980166b877
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-08-11 20:29:57 -0400

    add cascading display settings

---
+++ Wordlp/Models/AppDimensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Wordlp.Models;
+
+public class AppDimensions
+{
+    public int Height { get; set; }
+    // public int Width { get; set; } // don't care about the width for anything currently
+}
